d667d564f2bc91b9f4d1b4cc0a8677465778db9d
DannFox <ulisesdaniel1705@gmail.com>
2025-05-15 23:45:56 -0600

    Reapply "Remove image management functionality and related files"
    This reverts commit 273b7512b290246d19e13832751495bdea1c5eb8.

---
+++ Inventario.Infraestructure/Migrations/20250516042407_RemoveImagenProducto.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventario.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveImagenProducto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Imagenes_Productos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Imagenes_Productos",
+                columns: table => new
+                {
+                    id_imagen = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    id_producto = table.Column<int>(type: "int", nullable: false),
+                    url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imagenes_Productos", x => x.id_imagen);
+                    table.ForeignKey(
+                        name: "FK_Imagenes_Productos_Productos_id_producto",
+                        column: x => x.id_producto,
+                        principalTable: "Productos",
+                        principalColumn: "id_producto",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imagenes_Productos_id_producto",
+                table: "Imagenes_Productos",
+                column: "id_producto");
+        }
+    }
+}
